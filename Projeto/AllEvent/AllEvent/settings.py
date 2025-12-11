867a08908418e2b3defbfe0a781cee2a1e3fd99c
Vinicius R. <viniramos1000@gmail.com>
2025-12-11 21:56:16 +0000

    ACHO que vai

--- Projeto/AllEvent/AllEvent/settings.py
+++ Projeto/AllEvent/AllEvent/settings.py
@@ -115,7 +115,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True   
 EMAIL_USE_SSL = False  
 EMAIL_TIMEOUT = 30 
